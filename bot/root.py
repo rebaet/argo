a94a8e641b22dc2e849994712d02702e2e1f2df1
Rubaiyet Masum <64646035+rubaiyetcancode@users.noreply.github.com>
2021-04-16 14:01:17 +0600

    Account SIgn-Up

--- bot/root.py
+++ bot/root.py
@@ -1,23 +1,60 @@
+import os
 import discord
-import os
-
-from configparser import ConfigParser
-import os
+import pickle
 
 client = discord.Client()
+with open('bot/dat.txt', 'rb') as file:
+  dat = pickle.load(file)
+
+def save_changes_to_dat():
+  with open('bot/dat.txt', 'wb') as file:
+    pickle.dump(dat, file)
 
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+  print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-      return
+  if message.author == client.user:
+    return
 
-    call = message.content
+  call = message.content
+  user = message.author
 
 
+  # BOT PREFIX
+  if call.startswith("argo"):  
+    
+    # LOOK FOR COMMAND
+    if len(call.strip().split()) >> 1:    
+      command = call.strip().split()[1]
 
-client.run(os.environ.get('DISCORD_TOKEN'))  
+
+    # OPEN A NEW ACCOUNT
+    if command == 'newacc':
+      if user.id not in dat:    
+        dat[user.id] = {'wallet':0}
+        await message.channel.send("account created")
+        save_changes_to_dat()
+      else:
+        await message.channel.send("account already exits")
+
+    # CHECK ACCOUNT
+    if command == 'check':      
+      if user.id in dat:
+        await message.channel.send("account exists")
+      else:
+        await message.channel.send("account not found. \nenter 'argo newacc' to create one")
+
+    # CHECK WALLET
+    if command == 'wallet':
+      if user.id in dat:
+            await message.channel.send("you own " + str(dat[user.id]['wallet']) + ' coins')
+      else:
+        await message.channel.send("account not found. \nenter 'argo newacc' to create one")
+          
+
+#client.run(os.environ.get('DISCORD_TOKEN'))  
+client.run('ODMwNzgwMDA2Njg2OTE2NjE5.YHLqLA.fIJwBqv8E85Ns32QG6KC7cblJPQ')
